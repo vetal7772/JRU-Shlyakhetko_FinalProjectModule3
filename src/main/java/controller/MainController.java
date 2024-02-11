5b8414a5720d038b61cea601c6efcbd0f6ca8047
vit_s <vit_shl@ukr.net>
2024-02-11 23:44:21 +0200

    Add another dependencies in the pom.xml

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,11 @@
+//package controller;
+//
+//@Controller
+//public class MainController {
+//
+//    @GetMapping("/")
+//    public String viewIndexPage(Model model) {
+//        model.addAttribute("header", "Maven Generate War");
+//        return "index";
+//    }
+//}
